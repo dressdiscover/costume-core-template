25e61c26453456ca0e26bb39c28e5968060e6d91
Minor Gordon <minorg@server.fake>
2023-04-26 14:43:46 -0400

    update dev.sh against paradicms

--- dev.sh
+++ dev.sh
@@ -5,18 +5,18 @@ set -e
 cd `dirname "$(readlink -f "$0")"`
 
 PROJECTS_DIR_PATH=$PWD/../..
-DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/costume-core-template
+DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/costume_core_template
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/airtable-etl-action/action.py \
   --cache-directory-path $DATA_DIR_PATH/cached \
   --loaded-data-directory-path $DATA_DIR_PATH/loaded \
-  --pipeline-id costume-core-template \
+  --pipeline-id costume_core_template \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
   --cache-directory-path $DATA_DIR_PATH/cached \
   --data-path $DATA_DIR_PATH/loaded \
   --dev \
-  --pipeline-id costume-core-template
+  --pipeline-id costume_core_template
