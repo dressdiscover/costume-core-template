fb40cc1ef8957eeb78b146323138e32605693fdc
Minor Gordon <minorg@server.fake>
2023-04-26 15:14:56 -0400

    cached -> cache

--- dev.sh
+++ dev.sh
@@ -10,13 +10,13 @@ DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/costume_core_template
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/airtable-etl-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cached \
+  --cache-directory-path $DATA_DIR_PATH/cache \
   --loaded-data-directory-path $DATA_DIR_PATH/loaded \
   --pipeline-id costume_core_template \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cached \
+  --cache-directory-path $DATA_DIR_PATH/cache \
   --data-path $DATA_DIR_PATH/loaded \
   --dev \
   --pipeline-id costume_core_template
