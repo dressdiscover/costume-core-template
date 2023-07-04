51f3432d1bc42da085c24d4b55ecef272f6392e0
Minor Gordon <minorg@server.fake>
2023-07-04 11:48:43 -0400

    use .paradicms directory

--- dev.sh
+++ dev.sh
@@ -4,19 +4,21 @@ set -e
 
 cd `dirname "$(readlink -f "$0")"`
 
+THIS_DIR_PATH=$PWD
+CACHE_DIR_PATH=$THIS_DIR_PATH/.paradicms/cache
+DATA_DIR_PATH=$THIS_DIR_PATH/.paradicms/data
 PROJECTS_DIR_PATH=$PWD/../..
-DATA_DIR_PATH=$PROJECTS_DIR_PATH/paradicms/data/costume_core_template
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
 poetry run $PROJECTS_DIR_PATH/airtable-etl-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cache \
-  --loaded-data-directory-path $DATA_DIR_PATH/loaded \
+  --cache-directory-path $CACHE_DIR_PATH \
+  --loaded-data-directory-path $DATA_DIR_PATH \
   --pipeline-id costume_core_template \
   "$@"
 
 poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
-  --cache-directory-path $DATA_DIR_PATH/cache \
-  --data-path $DATA_DIR_PATH/loaded \
+  --cache-directory-path $CACHE_DIR_PATH \
+  --data-path $DATA_DIR_PATH \
   --dev \
   --pipeline-id costume_core_template
