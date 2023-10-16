0e9175540836877318d59ed09ae243939fcf8247
Minor Gordon <minorg@server.fake>
2023-10-16 09:46:52 -0400

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -7,18 +7,19 @@ cd `dirname "$(readlink -f "$0")"`
 THIS_DIR_PATH=$PWD
 CACHE_DIR_PATH=$THIS_DIR_PATH/.paradicms/cache
 DATA_DIR_PATH=$THIS_DIR_PATH/.paradicms/data
-PROJECTS_DIR_PATH=$PWD/../..
+PROJECTS_DIR_PATH=$PWD/..
 
 cd $PROJECTS_DIR_PATH/paradicms/lib/py/ssg
 
-poetry run $PROJECTS_DIR_PATH/airtable-etl-action/action.py \
-  --cache-directory-path $PROJECTS_DIR_PATH/paradicms/data \
+poetry run $PROJECTS_DIR_PATH/paradicms-airtable-etl-action/action.py \
+  --airtable-base-id appgU92SdGTwPIVNg \
+  --cache-directory-path $CACHE_DIR_PATH \
   --loaded-data-directory-path $DATA_DIR_PATH \
   --pipeline-id costume_core_template \
   "$@"
 
-poetry run $PROJECTS_DIR_PATH/ssg-action/action.py \
+poetry run $PROJECTS_DIR_PATH/paradicms-ssg-action/action.py \
   --cache-directory-path $CACHE_DIR_PATH \
   --data-path $DATA_DIR_PATH \
-  --dev \
+  --next-commands dev \
   --pipeline-id costume_core_template
